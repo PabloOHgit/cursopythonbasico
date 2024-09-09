46043e1d68618a10ae7ea9113a7fe588e7bbf2c7
Your Name <pablo.ortiz.hernaiz@gmail.com>
2024-09-09 19:51:18 +0200

    clase2

--- Unidad2/Untitled-3.py
+++ Unidad2/Untitled-3.py
@@ -1,3 +1,14 @@
 texto = "pruebas de cadenas"
+indice1 = texto.find("a", 6)
+print(indice1)
 
 print(texto[:6])
+
+print(texto[-10::1])
+
+numero = 33223
+num1 = 1
+print(f"El numero es {numero,num1}"+f"{num1}")
+
+indice = texto.find("de")
+print(indice)
