7e04fd0736ed97e2a7566c98bda0224355446b4e
PabloOHgit <pablo.ortiz.hernaiz@gmail.com>
2024-09-23 14:10:25 +0200

    23sepCasa3

---
+++ Unidad2/EjerciciosDiccionarios/ejer2.py
@@ -0,0 +1,13 @@
+# Ejercicio 2:
+# A. Crear un diccionario de estudiantes con sus calificaciones.
+# Ej. Juan = Matemáticas: 9, Historia: 8, Lengua: 7
+# B. Acceder a una calificación específica.
+# C. Mostrar el contenido del diccionario para que quede de la siguiente manera:
+# D. Elimina a Ana del diccionario.
+
+# A Diccionario de estudiandos y notas
+
+notas_estudiantes = {"Juan":{"Matemáticas":9,"Historia":8,"Lengua":7}},{"Pablo":{"Matemáticas":10,"Historia":9,"Lengua":8}}
+
+for calificacion in notas_estudiantes:
+    print(calificacion{"Matemáticas"})
