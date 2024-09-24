df4ab3ac08e8bf21b5e3f00377f1ad4a5f35da7f
Your Name <pablo.ortiz.hernaiz@gmail.com>
2024-09-24 16:51:04 +0200

    24sep1

--- Unidad2/EjerciciosDiccionariosListas/ejer1.py
+++ Unidad2/EjerciciosDiccionariosListas/ejer1.py
@@ -1,39 +1,34 @@
-cadena = "Pais;oro;plata;bronce\nEstados Unidos de América;40;44;42\nRepública Popular de China;40;27;24\nJapón;20;12;13"
+datos_olimpiadas = """Pais;oro;plata;bronce\nEstados Unidos de América;40;44;42\nRepública Popular de China;40;27;24\nJapón;20;12;13"""
 
-datos_olimpicos = cadena.split("\n")
+# Creamos una lista en datos_olimpicos con 4 trozos al dividir con \n
+datos_olimpicos = datos_olimpiadas.split("\n")
+# Creamos el diccionario final, vacío, que queremos crear con los datos
+medallero_general = {}
 
-# pais,oro,plata,bronce = cadena.split("\n")
+# for pais_medallas in datos_olimpicos[1:]:
+#     lista_pais_medallas = pais_medallas.split(";")
+#     # print(lista_pais_medallas)
+#     medallero = {"oro":lista_pais_medallas[1],"plata":lista_pais_medallas[2],"bronce":lista_pais_medallas[3]
+#     }
+#     medallero_general[lista_pais_medallas[0]] = medallero
+# print(medallero_general)
 
-
-# datos2 = datos.split(";")
-medallero = {}
+################# OTRA MANERA ############### LA QUE MAS ME GUSTA
     
-print(datos_olimpicos)
-pais,oro,plata,bronce = datos_olimpicos[0].split(";")
-print(pais,oro,plata,bronce)
-
 for pais_medallas in datos_olimpicos[1:]:
-    print(pais_medallas)
-    split = pais_medallas.split(";")
-    print(split)
-    
+    lista_pais_medallas = pais_medallas.split(";")
+    medallero_general[lista_pais_medallas[0]] = dict(oro = lista_pais_medallas[1],plata = lista_pais_medallas[2],bronce = lista_pais_medallas[3])
 
+print(medallero_general)
 
+################# OTRA MANERA ###############
 
+# medallero_pais = {}
+# lista_informacion_paises = datos_olimpiadas.splitlines()
 
+# datos_medallero = lista_informacion_paises[0].split(";")
 
-
-# for i in range(3):
-#     fila = []
-#     for j in range(3):
-#         valor = int(input(f"Introduce el valor para la posición [{i+1},{j+1}]: "))
-#         fila.append(valor)  # Añadir el valor a la fila
-#         print(fila)
-#     matriz.append(fila)  # Añadir la fila completa a la matriz
-#     print(matriz)
-# print(matriz)
-
-# print(datos[0])
-# print(datos[1])
-# print(datos[2])
-# print(datos[3])
+# for pais in lista_informacion_paises[1:]:
+#     pais,oro,plata,bronce = pais.split(";")
+#     medallero_pais[pais] = {datos_medallero[1]:oro,"plata":plata,"bronce":bronce}
+# print(medallero_pais)
