bc2d0f89340b6a4d819c799802c82a5ab7ac607e
Your Name <pablo.ortiz.hernaiz@gmail.com>
2024-09-25 16:34:16 +0200

    25sep1

---
+++ Unidad3/EjercicioClase/fun_nombre_edad.py
@@ -0,0 +1,12 @@
+# Crea una función que pregunte el nombre y la edad del usuario, y la
+# muestre por pantalla.
+
+def nombre_edad():
+    nombre = input("¿Cómo te llamas? ")
+    edad = input("¿Cuántos años tienes? ")
+    print(f"Te llamas {nombre} y tienes {edad} años.")
+    
+nombre_edad()
+
+
+    
