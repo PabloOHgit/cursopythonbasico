8d0e2496c259d714c8543bd05eadccbf159271c3
Your Name <pablo.ortiz.hernaiz@gmail.com>
2024-09-30 19:13:05 +0200

    30sep2

--- Unidad3/EjerciciosFuncionesII/ejer1.py
+++ Unidad3/EjerciciosFuncionesII/ejer1.py
@@ -5,3 +5,17 @@
 # b. La media
 # c. El número mayor y menor de la lista
 
+lista = [1,2,3,4]
+
+def funcion_multiple(lista):
+    lista = [1,2,3,4]
+    suma = sum(lista)
+    media = suma / len(lista)
+    num_mayor = max(lista)
+    num_menor = min(lista)
+    print(f"La suma de todos los elementos de la lista: {suma}")
+    print(f"La media de la lista: {media}")
+    print(f"El mayor de de la lista: {num_mayor}")
+    print(f"El menor de la lista: {num_menor}")
+
+funcion_multiple(lista)
--- Unidad3/EjerciciosFuncionesII/ejer2.py
+++ Unidad3/EjerciciosFuncionesII/ejer2.py
@@ -7,4 +7,27 @@
 # minúsculas)
 # c. Se puede elegir el desplazamiento que se quiera
 
+#CIFRADO CESAR
 
+def cifrado_cesar(mensaje,desplazamiento):
+    alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    mensaje_cifrado = ""
+    
+    for letra in mensaje.upper():
+        if letra in alfabeto:
+            indice = (alfabeto.index(letra)+desplazamiento) %26
+            mensaje_cifrado += alfabeto[indice]
+        else:
+            mensaje_cifrado += letra
+        
+    return mensaje_cifrado
+
+def descifrar_cesar(mensaje,desplazamiento):
+    return cifrado_cesar(mensaje,-desplazamiento)
+    
+mensaje = input("Introduce mensaje: ")
+desplazamiento = int(input("Introduce desplazamiento: "))
+
+print(cifrado_cesar(mensaje,desplazamiento))
+mensaje_descifrar = cifrado_cesar(mensaje,desplazamiento)
+print(descifrar_cesar(mensaje_descifrar,desplazamiento))
