e53a425d8939e89fadda75e7a4b823858d3b85e5
PabloOHgit <pablo.ortiz.hernaiz@gmail.com>
2024-10-07 15:14:04 +0200

    7octCasa

--- Unidad4/EjerciciosRepaso/ejercicio5.py
+++ Unidad4/EjerciciosRepaso/ejercicio5.py
@@ -23,9 +23,49 @@ for pais in paises_capitales:
     paises_lista.append(pais)
 
 pais_random = random.choice(paises_lista)
-print(pais_random)
+# print(pais_random)
 
-capital_usuario = input(f"Introduce el nombre de la capital para el pais {pais_random}: ")
+capital_usuario = ""
 
-if capital_usuario.capitalize() == paises_capitales[pais_random]:
-    print("Muy bien has acertado")
+def menu():
+    print("\n***** JUEGO DE ADIVINAR CAPITALES ****")
+    print("\nDispones de 3 intentos...juega bien")
+    global capital_usuario
+    capital_usuario = input(f"\nIntroduce el nombre de la capital para el pais {pais_random}: ")
+    return capital_usuario
+
+def repite():
+    global capital_usuario
+    capital_usuario = input(f"\nIntenta adivinar de nuevo el nombre de la capital para el pais {pais_random}: ")
+
+def comprueba(capital_usuario):
+    if capital_usuario.capitalize() == paises_capitales[pais_random]:
+        return True
+    
+
+menu()
+while True:
+    contador = 1
+    print(capital_usuario)
+    if comprueba(capital_usuario):
+        print("\nMuy bien has acertado")
+        break
+    else:
+        if contador == 1:
+            print(f"\nTe quedan {3-contador} intentos")
+            repite()
+            contador += 1
+            print(capital_usuario)
+            print(contador)
+        elif contador == 2:
+            print(f"\nTe queda {3-contador} intento")
+            repite()
+            contador += 1
+            print(capital_usuario)
+            print(contador)
+        else:
+            print(f"\nNO te quedan intentos")
+            print("\nHAS PERDIDO")
+            print(capital_usuario)
+            print(contador)
+
