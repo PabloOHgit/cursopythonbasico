a530de9d44fac566d4b4b6f3b8f64f6e885cdf56
Your Name <pablo.ortiz.hernaiz@gmail.com>
2024-10-08 16:35:58 +0200

    8oct1

---
+++ Unidad5/coche.py
@@ -0,0 +1,9 @@
+class Coche:
+    encendido = False
+    
+    def __init__(self,marca,modelo,color,puertas=5):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+        self.puertas = puertas
+        
